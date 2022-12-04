ce45f03f3fddf76ff2489ab17667142d2559a83f
UlianaFomina <6758599@gmail.com>
2022-12-04 14:07:15 +0400

    add create ad page

--- src/pages/createAd/index.js
+++ src/pages/createAd/index.js
@@ -1,12 +1,48 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Header } from "../../components/header";
 import "./index.css";
 
 export const CreateAd = ({ ...props }) => {
+  const { register, handleSubmit } = useForm();
   return (
     <div className="ad">
       <Header />
-      hiiiiiii
+      <div className="create-ad">
+        <div className="create-ad-box">
+          <p className="create-ad-title">create</p>
+          <p className="create-ad-title">your ad</p>
+        </div>
+        <form
+          className="create-ad-form"
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
+          <input
+            {...register("title")}
+            placeholder="title"
+            minLength={3}
+            className="create-ad-input"
+            required
+          />
+          <textarea
+            {...register("desk")}
+            placeholder="deskription"
+            minLength={10}
+            className="create-ad-input create-ad-textarea"
+            required
+          />
+          <input
+            {...register("img")}
+            placeholder="img"
+            type="file"
+            accept="image/*"
+            className="create-ad-input create-ad-file"
+          />
+          <button type="submit" className="create-ad-btn">create</button>
+        </form>
+      </div>
     </div>
   );
 };
